37f18f626815aeefea85b71199166ff845ad3f39
Mehmet Can ÇALIŞKAN <caliskanmehmetc@gmail.com>
2025-08-23 16:10:37 +0300

    diziler güncellendi

--- src/pages/tv/top_rated.vue
+++ src/pages/tv/top_rated.vue
@@ -3,24 +3,65 @@ import { ref, onMounted } from 'vue';
 import Sidebar from '../../components/Sidebar.vue';
 import { topRatedTVShows } from '../../services/diziService.js';
 import MovieCard from '../../components/MovieCard.vue';
-const tvshows = ref([]);
+
+const originaltvshows = ref([]);
+const tvshows =ref([]);
 const loading = ref(true);
 
 onMounted(async () => {
   try {
-    tvshows.value = await topRatedTVShows();
+    originaltvshows.value = await topRatedTVShows();
+    tvshows.value=[...originaltvshows.value];
   } catch (err) {
     console.error('Dizi verisi alınamadı', err);
   } finally {
     loading.value = false;
   }
 });
-</script>
 
+const applySort=(sort)=>{
+    switch(sort) {
+    case 'Popülerliğe Göre Azalan':
+    tvshows.value.sort((a, b) => b.popularity - a.popularity);
+    break;
+    case 'Popülerliğe Göre Artan':
+    tvshows.value.sort((a, b) => a.popularity - b.popularity);
+        break;
+
+    case 'En Yeni':
+    tvshows.value.sort((a, b) => new Date(b.first_air_date) - new Date(a.first_air_date));
+        break;
+
+    case 'En Eski':
+    tvshows.value.sort((a, b) => new Date(a.first_air_date) - new Date(b.first_air_date));
+        break;
+
+    case 'En Yüksek Oy Alan':
+    tvshows.value.sort((a, b) => b.vote_average - a.vote_average);
+        break;
+
+    case 'En Düşük Oy Alan':
+    tvshows.value.sort((a, b) => a.vote_average - b.vote_average);
+        break;
+
+  }
+};
+const applyFilter = (filters) => {
+  tvshows.value = originaltvshows.value.filter(tvshows => {
+    if (filters.action && tvshows.genre_ids.includes(28)) return true;
+    if (filters.drama && tvshows.genre_ids.includes(18)) return true;
+    if (filters.comedy && tvshows.genre_ids.includes(35)) return true;
+    return !filters.action && !filters.drama && !filters.comedy; 
+  });
+};
+</script>
 <template>
   <div class="flex min-h-screen">
 
-    <Sidebar title="En Fazla Oy Alan Diziler" />
+    <Sidebar title="En Fazla Oy Alan Diziler" 
+    @sort-changed="applySort"
+    @filter-changed="applyFilter"
+    />
 
     <div class="flex-1 p-6 grid grid-cols-5 gap-6">
       <div v-if="loading" class="text-center col-span-full">Yükleniyor...</div>
